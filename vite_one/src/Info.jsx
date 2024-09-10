785d4c75f703f793e23369428a43accdbce51981
devp45 <devpatel4829@gmail.com>
2024-09-10 23:49:44 +0530

    Add reset button to reset counter to zero

--- vite_one/src/Info.jsx
+++ vite_one/src/Info.jsx
@@ -14,13 +14,21 @@ function Info() {
         setcounter(counter-1)
         console.log(counter)
     }
+    const reset = () =>{
+        setcounter(0)
+        console.log(counter)
+    }
 
     return (
         <div>
             <h3>Counter: {counter}</h3>
             <button onClick={addOne}>Add one in {counter}</button>
             <br />
+            <br />
             <button onClick={minusOne}>Minus one in {counter}</button>
+            <br />
+            <br />
+            <button onClick={reset}>Reset</button>
         </div>
     )
 }
